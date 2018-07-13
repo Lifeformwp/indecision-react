3f6f6cc7ab48a3a81c22e97da1a075f70122804e
Serhii Kondratiuk <vielon.indie@gmail.com>
2018-07-13 15:53:43 +0300

    Dev final (#2)
    Final improvements

--- src/components/Indecision.js
+++ src/components/Indecision.js
@@ -3,18 +3,51 @@ import AddDecision from'./AddDecision'
 import Header from './Header'
 import Action from './Action'
 import Decisions from './Decisions'
+import DecisionModal from './DecisionModal'
 
 export default class Indecision extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDeleteDecisions = this.handleDeleteDecisions.bind(this);
-        this.handleAddDecision = this.handleAddDecision.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleDeleteDecisionSingular = this.handleDeleteDecisionSingular.bind(this);
-        this.state = {
+    state = {
+        decisions: [],
+        selectedDecision: undefined
+    };
+
+    handleDeleteDecisions = () => {
+        this.setState(() => ({
             decisions: []
-        };
-    }
+        }));
+    };
+
+    handleDeleteDecisionSingular = (decisionToRemoving) => {
+        this.setState((prevState) => ({
+            decisions: prevState.decisions.filter((decision) => decisionToRemoving !== decision)
+        }))
+    };
+
+    handlePick = () => {
+        const randomNum = Math.floor(Math.random() * this.state.decisions.length);
+        const decision = this.state.decisions[randomNum];
+        this.setState(() => ({
+            selectedDecision: decision
+        }));
+    };
+
+    handleAddDecision = (decision) => {
+        if (!decision) {
+            return 'Enter valid data there';
+        } else if (this.state.decisions.indexOf(decision) > -1) {
+            return 'This decision is already in the decisions list';
+        }
+
+        this.setState((prevState) => ({
+            decisions: prevState.decisions.concat(decision)
+        }));
+    };
+
+    handleDeleteSelectedDecision = () => {
+        this.setState(() => ({
+            selectedDecision: undefined
+        }))
+    };
 
     componentDidMount() {
         try {
@@ -36,53 +69,31 @@ export default class Indecision extends React.Component {
         }
     }
 
-    handleDeleteDecisions() {
-        this.setState(() => ({
-            decisions: []
-        }));
-    }
-
-    handleDeleteDecisionSingular(decisionToRemoving) {
-        this.setState((prevState) => ({
-            decisions: prevState.decisions.filter((decision) => decisionToRemoving !== decision)
-        }))
-    }
-
-    handlePick() {
-        const randomNum = Math.floor(Math.random() * this.state.decisions.length);
-        const decision = this.state.decisions[randomNum];
-        alert(decision);
-    }
-
-    handleAddDecision(decision) {
-        if (!decision) {
-            return 'Enter valid data there';
-        } else if (this.state.decisions.indexOf(decision) > -1) {
-            return 'This decision is already in the decisions list';
-        }
-
-        this.setState((prevState) => ({
-            decisions: prevState.decisions.concat(decision)
-        }));
-    }
-
     render() {
         const subtitled = 'Put text in input of this website please';
 
         return (
             <div>
                 <Header subtitled={subtitled} />
-                <Action
-                    hasDecisions={this.state.decisions.length > 0}
-                    handlePick={this.handlePick}
-                />
-                <Decisions
-                    decisions={this.state.decisions}
-                    handleDeleteDecisions={this.handleDeleteDecisions}
-                    handleDeleteDecisionSingular={this.handleDeleteDecisionSingular}
-                />
-                <AddDecision
-                    handleAddDecision={this.handleAddDecision}
+                <div className="container">
+                    <Action
+                        hasDecisions={this.state.decisions.length > 0}
+                        handlePick={this.handlePick}
+                    />
+                    <div className="widget">
+                        <Decisions
+                            decisions={this.state.decisions}
+                            handleDeleteDecisions={this.handleDeleteDecisions}
+                            handleDeleteDecisionSingular={this.handleDeleteDecisionSingular}
+                        />
+                        <AddDecision
+                            handleAddDecision={this.handleAddDecision}
+                        />
+                    </div>
+                </div>
+                <DecisionModal
+                    selectedDecision={this.state.selectedDecision}
+                    handleDeleteSelectedDecision={this.handleDeleteSelectedDecision}
                 />
             </div>
         )
